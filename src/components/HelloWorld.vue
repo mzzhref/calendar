58f8aee8ca79296a117ff0606383ff7343e0c34d
mzzhref <mzzhref@163.com>
2019-03-15 20:08:23 +0800

    暂存

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,159 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="box">
+    <div id="week">
+      <a class="week" href="javascript:;" @click="prev">prev</a>
+      <a class="week" href="javascript:;" v-for="(el,index) in week" :key="index">{{ el }}</a>
+      <a class="week" href="javascript:;" @click="next">next</a>
+    </div>
+    <div id="calendar">
+      <a
+        class="calendar"
+        href="javascript:;"
+        v-for="(el,index) in data"
+        :key="index"
+        :class="{'calendar-active': el == 15?true:false}"
+      >{{ el }}</a>
+    </div>
   </div>
 </template>
 
 <script>
+var vm = null;
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      week: ["日", "一", "二", "三", "四", "五", "六"],
+      data: [
+        "25",
+        "26",
+        "27",
+        "28",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "11",
+        "12",
+        "13",
+        "14",
+        "15",
+        "16",
+        "17",
+        "18",
+        "19",
+        "20",
+        "21",
+        "22",
+        "23",
+        "24",
+        "25",
+        "26",
+        "27",
+        "28",
+        "29",
+        "30",
+        "31",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7"
+      ]
+    };
+  },
+  created() {
+    vm = this;
+    this.monthFirWeek(new Date());
+  },
+  methods: {
+    init() {},
+    prev() {
+      console.log("prev");
+    },
+    next() {
+      console.log("next");
+    },
+    monthFirWeek(date) {
+      console.log(date);
+      var month = date.getMonth() + 1;
+      console.log(month);
+
+      date.setDate(1);
+      var today = date.getDay();
+      console.log(today);
+    },
+    timeFormat(date) {
+      var y = date.getFullYear();
+      var m = date.getMonth() + 1;
+      var d = date.getDate();
+      return y + "-" + m + "-" + d;
     }
-  }
-}
+  },
+  mounted() {}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.box {
+  width: 100%;
+  overflow: hidden;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+#week {
+  width: 440px;
+  height: 60px;
+  margin: 100px auto 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.week {
+  display: block;
+  width: 40px;
+  height: 40px;
+  float: left;
+  background: #ccc;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  color: #000;
+  line-height: 40px;
+  text-align: center;
 }
-a {
-  color: #42b983;
+.week-active {
+  background: #000;
+  color: #fff;
+}
+.week:nth-of-type(9n) {
+  margin-right: 0;
+}
+#calendar {
+  width: 340px;
+  height: 290px;
+  margin: 0 auto;
+}
+.calendar {
+  display: block;
+  width: 40px;
+  height: 40px;
+  float: left;
+  background: #ccc;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  color: #000;
+  line-height: 40px;
+  text-align: center;
+}
+.calendar-active {
+  background: #000;
+  color: #fff;
+}
+.calendar:nth-of-type(7n) {
+  margin-right: 0;
 }
 </style>
