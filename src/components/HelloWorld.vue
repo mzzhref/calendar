046995b81c91872910b9e0d1480f04fbc18b4a16
mzzhref <mzzhref@163.com>
2019-03-18 14:58:42 +0800

    提交代码

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,12 +7,27 @@
     </div>
     <div id="calendar">
       <a
+        class="calendar calendar-else"
+        href="javascript:;"
+        v-for="(prev,index1) in prevData"
+        :key="index1"
+        v-if="(index1+1)<weekNum"
+      >{{ prevData[prevData.length - (weekNum - index1 - 1)] }}</a>
+      <a
         class="calendar"
         href="javascript:;"
-        v-for="(el,index) in data"
-        :key="index"
-        :class="{'calendar-active': el == 15?true:false}"
+        v-for="(el,index2) in thisData"
+        :key="index2"
+        :class="{'calendar-active': el == today?true:false}"
       >{{ el }}</a>
+      <a
+        class="calendar calendar-else"
+        href="javascript:;"
+        v-for="(next,index3) in nextData"
+        :key="index3"
+        v-bind:data-key="index3"
+        v-if="index3 < (42 - thisData.length - (weekNum - 1))"
+      >{{ next }}</a>
     </div>
   </div>
 </template>
@@ -23,79 +38,78 @@ export default {
   name: "HelloWorld",
   data() {
     return {
-      week: ["日", "一", "二", "三", "四", "五", "六"],
-      data: [
-        "25",
-        "26",
-        "27",
-        "28",
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "10",
-        "11",
-        "12",
-        "13",
-        "14",
-        "15",
-        "16",
-        "17",
-        "18",
-        "19",
-        "20",
-        "21",
-        "22",
-        "23",
-        "24",
-        "25",
-        "26",
-        "27",
-        "28",
-        "29",
-        "30",
-        "31",
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7"
-      ]
+      week: ["一", "二", "三", "四", "五", "六", "日"],
+      year: new Date().getFullYear(),
+      month: new Date().getMonth()+1,
+      today: new Date().getDate(),
+      prevData: [],
+      thisData: [],
+      nextData: [],
+      weekNum: null
     };
   },
   created() {
     vm = this;
-    this.monthFirWeek(new Date());
+    this.init();
   },
   methods: {
-    init() {},
+    init() {
+      vm.prevData = [];
+      vm.thisData = [];
+      vm.nextData = [];
+      this.monthFirWeek(new Date(vm.year, vm.month-1, 1));
+      this.monthDayData(this.thisMonthLastDay(this.year, this.month-1), 'prev');
+      this.monthDayData(this.thisMonthLastDay(this.year, this.month), 'this');
+      this.monthDayData(this.thisMonthLastDay(this.year, this.month+1), 'next');
+    },
     prev() {
-      console.log("prev");
+      this.month--;
+      if(this.month == 0){
+        this.year--;
+        this.month = 12;
+      }
+      this.init();
     },
     next() {
-      console.log("next");
+      this.month++;
+      if(this.month == 13){
+        this.year++;
+        this.month = 1;
+      }
+      this.init();
     },
+    // 每月第一天是周几
     monthFirWeek(date) {
-      console.log(date);
-      var month = date.getMonth() + 1;
-      console.log(month);
-
+      console.log(this.timeFormat(date))
       date.setDate(1);
-      var today = date.getDay();
-      console.log(today);
+      var num = date.getDay();
+      if(num == 0){
+        num == 7
+      }
+      this.weekNum = num;
     },
+    // 每月最后一天
+    thisMonthLastDay(year, month) {
+      return new Date(year, month, 0).getDate();
+    },
+    // 每月的天数数组
+    monthDayData(day, btn){
+      for(var i =0; i<day; i++){
+        if(btn == 'prev'){
+          vm.prevData.push(i+1);
+        }else if(btn == 'this'){
+          vm.thisData.push(i+1);
+        }else if(btn == 'next'){
+          vm.nextData.push(i+1);
+        } 
+      }
+    },
+    // 日期格式化
     timeFormat(date) {
       var y = date.getFullYear();
       var m = date.getMonth() + 1;
       var d = date.getDate();
-      return y + "-" + m + "-" + d;
+      return y + "/" + m + "/" + d;
     }
   },
   mounted() {}
@@ -149,9 +163,12 @@ export default {
   line-height: 40px;
   text-align: center;
 }
-.calendar-active {
+.calendar-active{
   background: #000;
   color: #fff;
+}
+.calendar-else {
+  color: #eee;
 }
 .calendar:nth-of-type(7n) {
   margin-right: 0;
